0064564642de62b649805d2bc3d05ee3c0eba613
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-08-16 14:29:24 -0400

    meta title

--- elements/header.php
+++ elements/header.php
@@ -4,8 +4,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="noindex" />
   <link rel="icon" type="image/png" href="img/favicon.png" />
-  <title>Recipes</title>
+  <title>Daniels' Family Recipes</title>
   <link rel="stylesheet" type="text/css" href="css/ma2-style.css">
   <link rel="stylesheet" type="text/css" href="css/site.css">
 </head>
