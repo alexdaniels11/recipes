ac660e435efc36e1288e62578d4936f5b0096d14
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-08-16 14:17:43 -0400

    updating recipes

--- elements/footer.php
+++ elements/footer.php
@@ -2,7 +2,7 @@
 
   <div id="OverlayMask" class="ma-overlay-mask"></div>
 
-  <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
+  <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script type='text/javascript' src="js/site.js"></script>
 
 </body>
--- index.php
+++ index.php
@@ -3,16 +3,98 @@
 	<div class="ma-container">
 
     <h1>Daniels' Family Recipes</h1>
-    <p>Use the search bar to find the recipe you are looking for and then click the recipe you want to do. Inside each recipe you'll find ingredient with amounts and directions. Enjoy</p>
+    <p>Use the search bar to find the recipe you are looking for and then click the recipe you want to do. Inside each recipe you'll find ingredients with amounts and directions. Enjoy!</p>
 
     <input class="ma-input margin-0-0-8-0" type="text" id="searchExpandableTable" placeholder="Type to search table...">
     <table id="searchTable" class="ma-table ma-table-fullwidth ma-table-fixed expandable-table-data">
       <thead class="ma-table-header">
         <tr>
-          <th colspan="2">Daniels' Family Recipes</th>
+          <th colspan="2">List of Recipes</th>
         </tr>
       </thead>
       <tbody>
+
+
+
+
+
+      <!-- START -->
+        <tr class="expand-tr">
+          <td colspan="2" class="background--white">Butter Cream Scallops<span class="sign plus"></span></td>
+        </tr>
+        <tr class="sub-tr" style="display: none;">
+          <td>
+            <ul>
+              <li>Main
+                <ul>
+                  <li>5-6 Scallops</li>
+                  <li>Cooking Spray</li>
+                  <li>1/3 cup of White Wine (Pinot Grigio)</li>
+                  <li>1 tbs. of minced White Onion</li>
+                  <li>3 tbs. Chilled Butter</li>
+                  <li>1 1/2 tbs. of fresh Parsley</li>
+                  <li>1 1/2 tbs. of fresh Green Onion</li>
+                  <li>Salt &amp; Pepper</li>
+                </ul>
+              </li>
+            </ul>
+          </td>
+          <td>
+            <ol>
+              <li>Place skillet over medium-high heat &amp; coat scallops with cooking spray, salt &amp; pepper</li>
+              <li>Sear 3-5 minutes or until brown &amp; flip for 2-3 minutes</li>
+              <li>Remove &amp; keep warm in oven at 150&deg;-175&deg;</li>
+              <li>Add White Wine to pan, add White Onion &amp; boil for 5 minutes</li>
+              <li>Reduce to low heat &amp; whisk Butter in</li>
+              <li>Stir in Parsley, Green Onion &amp; Scallops and serve</li>
+            </ol>
+          </td>
+        </tr>
+      <!-- END -->
+
+
+
+
+
+      <!-- START -->
+        <tr class="expand-tr">
+          <td colspan="2" class="background--white">Buttermilk Roast Chicken with Garlic<span class="sign plus"></span></td>
+        </tr>
+        <tr class="sub-tr" style="display: none;">
+          <td>
+            <ul>
+              <li>Main
+                <ul>
+                  <li>6 Chicken Drumsticks</li>
+                </ul>
+              </li>
+              <li>Sauce
+                <ul>
+                  <li>1 cup of Buttermilk</li>
+                  <li>1 tbs. of Olive Oil</li>
+                  <li>5 minced Garlic Cloves</li>
+                  <li>1/6 bunch of fresh Parsley</li>
+                  <li>1/2 tsp. of salt</li>
+                  <li>1/2 tsp. of pepper</li>
+                  <li>1 gallon Ziplock Bag</li>
+                </ul>
+              </li>
+            </ul>
+          </td>
+          <td>
+            <ol>
+              <li>Combine all ingredients in a small bowl (minus Chicken Drumsticks) &amp; whisk until it is a marinade</li>
+              <li>Place Chicken Drumsticks in Ziplock Bag &amp; pour marinade over chicken; let marinate for 24 hours</li>
+              <li>Once Chicken Drumsticks have marinated for 24 hours, heat oven to 400&deg;</li>
+              <li>Place Chicken Drumsticks in oven dish &amp; pour marinade on top</li>
+              <li>Roast for 30-45 minutes until Chicken Drumsticks juice runs clean &amp; serve</li>
+            </ol>
+          </td>
+        </tr>
+      <!-- END -->
+
+
+      <!-- START -->
         <tr class="expand-tr">
           <td colspan="2" class="background--white">Creamy Parmesan Garlic Muchroom Chicken<span class="sign plus"></span></td>
         </tr>
@@ -29,12 +111,12 @@
           		</li>
           		<li>Sauce
           			<ul>
-          				<li>.25 cup of butter</li>
+          				<li>1/4 cup of butter</li>
               		<li>2 Minced Garlic Cloves</li>
               		<li>1 tbs. of Flour</li>
-              		<li>.5 cup of Chicken Broth</li>
+              		<li>1/2 cup of Chicken Broth</li>
               		<li>1 cup of Heavy Cream or Half &amp; Half</li>
-              		<li>.5 cup of Grated Parmesan Cheese</li>
+              		<li>1/2 cup of Grated Parmesan Cheese</li>
           				<li>Salt &amp; Pepper</li>
           				<li>1 cup of Chopped Spinach</li>
           			</ul>
@@ -43,190 +125,22 @@
           </td>
           <td>
             <ol>
-              <li>Add Chicken &amp; Olive Oil and cook on medium-high for 3-5 minutes on each side; remove Chicken and place in bowl</li>
-              <li>Place Mushrooms in pan and cook until tender; pour Mushrooms &amp; Olive Oil into bowl with Chicken</li>
+              <li>Add Chicken &amp; Olive Oil &amp; cook on medium-high for 3-5 minutes on each side; remove Chicken &amp; place in bowl</li>
+              <li>Place Mushrooms in pan &amp; cook until tender; pour Mushrooms &amp; Olive Oil into bowl with Chicken</li>
               <li>Add Butter until melted</li>
               <li>Add minced Garlic &amp; cook with Butter until tender</li>
               <li>Whisk in Flour to thicken</li>
               <li>Once Flour has thickened sauce, Whisk in Broth, Heavy Cream (or Half &amp; Half), Parmesan Cheese, Salt, &amp; Pepper until smooth</li>
-              <li>Lower heat to medium-low on simmer and add Spinach to wilt</li>
-              <li>Once Spinach is wilted, add back into the pan Chicken, Mushrooms &amp; Olive Oil from the bowl and mix in the Mushrooms and Olive Oil evenly into the sauce</li>
-              <li>Let cook in pan for another 4-6 minutes and serve</li>
+              <li>Lower heat to medium-low on simmer &amp; add Spinach to wilt</li>
+              <li>Once Spinach is wilted, add back into the pan Chicken, Mushrooms &amp; Olive Oil from the bowl &amp; mix in the Mushrooms &amp; Olive Oil evenly into the sauce</li>
+              <li>Let cook in pan for another 4-6 minutes &amp; serve</li>
             </ol>
           </td>
         </tr>
+      <!-- END -->
 
-        <tr class="expand-tr">
-          <td colspan="2" class="background--white">Recipe 2<span class="sign plus"></span></td>
-        </tr>
-        <tr class="sub-tr" style="display: none;">
-          <td>
-            <ul>
-              <li>Main
-                <ul>
-                  <li>4 Boneless Chicken Breasts</li>
-                  <li>4 tbs. of Olive Oil</li>
-                  <li>Salt &amp; Pepper</li>
-                  <li>8 oz. of Sliced Mushrooms</li>
-                </ul>
-              </li>
-              <li>Sauce
-                <ul>
-                  <li>.25 cup of butter</li>
-                  <li>2 Minced Garlic Cloves</li>
-                  <li>1 tbs. of Flour</li>
-                  <li>.5 cup of Chicken Broth</li>
-                  <li>1 cup of Heavy Cream or Half &amp; Half</li>
-                  <li>.5 cup of Grated Parmesan Cheese</li>
-                  <li>Salt &amp; Pepper</li>
-                  <li>1 cup of Chopped Spinach</li>
-                </ul>
-              </li>
-            </ul>
-          </td>
-          <td>
-            <ol>
-              <li>Add Chicken &amp; Olive Oil and cook on medium-high for 3-5 minutes on each side; remove Chicken and place in bowl</li>
-              <li>Place Mushrooms in pan and cook until tender; pour Mushrooms &amp; Olive Oil into bowl with Chicken</li>
-              <li>Add Butter until melted</li>
-              <li>Add minced Garlic &amp; cook with Butter until tender</li>
-              <li>Whisk in Flour to thicken</li>
-              <li>Once Flour has thickened sauce, Whisk in Broth, Heavy Cream (or Half &amp; Half), Parmesan Cheese, Salt, &amp; Pepper until smooth</li>
-              <li>Lower heat to medium-low on simmer and add Spinach to wilt</li>
-              <li>Once Spinach is wilted, add back into the pan Chicken, Mushrooms &amp; Olive Oil from the bowl and mix in the Mushrooms and Olive Oil evenly into the sauce</li>
-              <li>Let cook in pan for another 4-6 minutes and serve</li>
-            </ol>
-          </td>
-        </tr>
 
-        <tr class="expand-tr">
-          <td colspan="2" class="background--white">Recipe 3<span class="sign plus"></span></td>
-        </tr>
-        <tr class="sub-tr" style="display: none;">
-          <td>
-            <ul>
-              <li>Main
-                <ul>
-                  <li>4 Boneless Chicken Breasts</li>
-                  <li>4 tbs. of Olive Oil</li>
-                  <li>Salt &amp; Pepper</li>
-                  <li>8 oz. of Sliced Mushrooms</li>
-                </ul>
-              </li>
-              <li>Sauce
-                <ul>
-                  <li>.25 cup of butter</li>
-                  <li>2 Minced Garlic Cloves</li>
-                  <li>1 tbs. of Flour</li>
-                  <li>.5 cup of Chicken Broth</li>
-                  <li>1 cup of Heavy Cream or Half &amp; Half</li>
-                  <li>.5 cup of Grated Parmesan Cheese</li>
-                  <li>Salt &amp; Pepper</li>
-                  <li>1 cup of Chopped Spinach</li>
-                </ul>
-              </li>
-            </ul>
-          </td>
-          <td>
-            <ol>
-              <li>Add Chicken &amp; Olive Oil and cook on medium-high for 3-5 minutes on each side; remove Chicken and place in bowl</li>
-              <li>Place Mushrooms in pan and cook until tender; pour Mushrooms &amp; Olive Oil into bowl with Chicken</li>
-              <li>Add Butter until melted</li>
-              <li>Add minced Garlic &amp; cook with Butter until tender</li>
-              <li>Whisk in Flour to thicken</li>
-              <li>Once Flour has thickened sauce, Whisk in Broth, Heavy Cream (or Half &amp; Half), Parmesan Cheese, Salt, &amp; Pepper until smooth</li>
-              <li>Lower heat to medium-low on simmer and add Spinach to wilt</li>
-              <li>Once Spinach is wilted, add back into the pan Chicken, Mushrooms &amp; Olive Oil from the bowl and mix in the Mushrooms and Olive Oil evenly into the sauce</li>
-              <li>Let cook in pan for another 4-6 minutes and serve</li>
-            </ol>
-          </td>
-        </tr>
 
-        <tr class="expand-tr">
-          <td colspan="2" class="background--white">Recipe 4<span class="sign plus"></span></td>
-        </tr>
-        <tr class="sub-tr" style="display: none;">
-          <td>
-            <ul>
-              <li>Main
-                <ul>
-                  <li>4 Boneless Chicken Breasts</li>
-                  <li>4 tbs. of Olive Oil</li>
-                  <li>Salt &amp; Pepper</li>
-                  <li>8 oz. of Sliced Mushrooms</li>
-                </ul>
-              </li>
-              <li>Sauce
-                <ul>
-                  <li>.25 cup of butter</li>
-                  <li>2 Minced Garlic Cloves</li>
-                  <li>1 tbs. of Flour</li>
-                  <li>.5 cup of Chicken Broth</li>
-                  <li>1 cup of Heavy Cream or Half &amp; Half</li>
-                  <li>.5 cup of Grated Parmesan Cheese</li>
-                  <li>Salt &amp; Pepper</li>
-                  <li>1 cup of Chopped Spinach</li>
-                </ul>
-              </li>
-            </ul>
-          </td>
-          <td>
-            <ol>
-              <li>Add Chicken &amp; Olive Oil and cook on medium-high for 3-5 minutes on each side; remove Chicken and place in bowl</li>
-              <li>Place Mushrooms in pan and cook until tender; pour Mushrooms &amp; Olive Oil into bowl with Chicken</li>
-              <li>Add Butter until melted</li>
-              <li>Add minced Garlic &amp; cook with Butter until tender</li>
-              <li>Whisk in Flour to thicken</li>
-              <li>Once Flour has thickened sauce, Whisk in Broth, Heavy Cream (or Half &amp; Half), Parmesan Cheese, Salt, &amp; Pepper until smooth</li>
-              <li>Lower heat to medium-low on simmer and add Spinach to wilt</li>
-              <li>Once Spinach is wilted, add back into the pan Chicken, Mushrooms &amp; Olive Oil from the bowl and mix in the Mushrooms and Olive Oil evenly into the sauce</li>
-              <li>Let cook in pan for another 4-6 minutes and serve</li>
-            </ol>
-          </td>
-        </tr>
-
-        <tr class="expand-tr">
-          <td colspan="2" class="background--white">Recipe 5<span class="sign plus"></span></td>
-        </tr>
-        <tr class="sub-tr" style="display: none;">
-          <td>
-            <ul>
-              <li>Main
-                <ul>
-                  <li>4 Boneless Chicken Breasts</li>
-                  <li>4 tbs. of Olive Oil</li>
-                  <li>Salt &amp; Pepper</li>
-                  <li>8 oz. of Sliced Mushrooms</li>
-                </ul>
-              </li>
-              <li>Sauce
-                <ul>
-                  <li>.25 cup of butter</li>
-                  <li>2 Minced Garlic Cloves</li>
-                  <li>1 tbs. of Flour</li>
-                  <li>.5 cup of Chicken Broth</li>
-                  <li>1 cup of Heavy Cream or Half &amp; Half</li>
-                  <li>.5 cup of Grated Parmesan Cheese</li>
-                  <li>Salt &amp; Pepper</li>
-                  <li>1 cup of Chopped Spinach</li>
-                </ul>
-              </li>
-            </ul>
-          </td>
-          <td>
-            <ol>
-              <li>Add Chicken &amp; Olive Oil and cook on medium-high for 3-5 minutes on each side; remove Chicken and place in bowl</li>
-              <li>Place Mushrooms in pan and cook until tender; pour Mushrooms &amp; Olive Oil into bowl with Chicken</li>
-              <li>Add Butter until melted</li>
-              <li>Add minced Garlic &amp; cook with Butter until tender</li>
-              <li>Whisk in Flour to thicken</li>
-              <li>Once Flour has thickened sauce, Whisk in Broth, Heavy Cream (or Half &amp; Half), Parmesan Cheese, Salt, &amp; Pepper until smooth</li>
-              <li>Lower heat to medium-low on simmer and add Spinach to wilt</li>
-              <li>Once Spinach is wilted, add back into the pan Chicken, Mushrooms &amp; Olive Oil from the bowl and mix in the Mushrooms and Olive Oil evenly into the sauce</li>
-              <li>Let cook in pan for another 4-6 minutes and serve</li>
-            </ol>
-          </td>
-        </tr>
       </tbody>
     </table>
 	</div>
