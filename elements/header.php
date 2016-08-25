19afe9ca38bee4a2059264d6793960491882f06f
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-08-25 16:25:19 -0400

    apple icons

--- elements/header.php
+++ elements/header.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="robots" content="noindex" />
-	<link rel="icon" type="image/png" href="img/favicon.png" />
+	<link rel="icon" type="image/png" href="img/apple-icon-144x144.png" />
 	<link rel="apple-touch-icon" type="image/png" href="img/apple-icon-144x144.png">
 
 	<title>Daniels' Family Recipes</title>
