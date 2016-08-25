aca399c49952972a6e80c23fc5d67ba66dad592a
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-08-25 16:21:19 -0400

    apple icons

--- elements/header.php
+++ elements/header.php
@@ -2,14 +2,17 @@
 <html>
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="robots" content="noindex" />
-  <link rel="icon" type="image/png" href="img/favicon.png" />
-  <title>Daniels' Family Recipes</title>
-  <link rel="stylesheet" type="text/css" href="css/ma2-style.css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/site.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex" />
+	<link rel="icon" type="image/png" href="img/favicon.png" />
+	<link rel="apple-touch-icon" type="image/png" href="http://static.tutsplus.com/assets/apple-touch-icon-8b86ba48e7f31535461f183680fe2ac9.png">
+
+	<title>Daniels' Family Recipes</title>
+
+	<link rel="stylesheet" type="text/css" href="css/ma2-style.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/site.css">
 </head>
 
 <body class="ma-global">
